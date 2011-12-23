0c855dc6eefccd4f72be93494a6d513752140faa
Michael Elsdörfer <michael@elsdoerfer.com>
2011-12-23 12:28:49 -0800

    Added persistance support to server, using shelve.
    lockfile is required because shelve can't lock on it's own.

--- server/app.py
+++ server/app.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
-import json, uuid
+import json, uuid, shelve
+from contextlib import contextmanager
+from lockfile import FileLock
 from flask import Flask, request
 from flask.helpers import make_response
 
+# Setup Flask app
 app = Flask(__name__)
 import config
 app.config.from_object(config)
@@ -14,6 +17,17 @@ if app.config.get('CLIENT_PATH'):
         '/': config.CLIENT_PATH
     })
 
+# Configure database
+@contextmanager
+def database():
+    dbfilename = app.config['DATABASE']
+    with FileLock(dbfilename):
+        db = shelve.open(dbfilename)
+        try:
+            yield db
+        finally:
+            db.close()
+
 
 def make_json_response(body, status_code=200):
     resp = make_response(json.dumps(body))
@@ -22,39 +36,41 @@ def make_json_response(body, status_code=200):
     return resp
 
 
-things = {}
-
 @app.route('/things/', methods=['GET'])
 def get_things():
-    data = things.values()
-    return make_json_response(data)
+    with database() as things:
+        data = things.values()
+        return make_json_response(data)
 
 @app.route('/things/<thing_id>',  methods=['GET'])
 def get_thing(thing_id):
-    try:
-        thing = things[thing_id]
-    except KeyError:
-        return make_json_response({'message': 'invalid id'}, 400)
-    return make_json_response(thing)
+    with database() as things:
+        try:
+            thing = things[thing_id]
+        except KeyError:
+            return make_json_response({'message': 'invalid id'}, 400)
+        return make_json_response(thing)
 
 @app.route('/things/', methods=['POST'])
 def create_thing():
-    new_thing = request.json
-    new_thing['id'] = uuid.uuid4().hex
-    things[new_thing['id']] = new_thing
-    return make_json_response(new_thing)
+    with database() as things:
+        new_thing = request.json
+        new_thing['id'] = uuid.uuid4().hex
+        things[new_thing['id']] = new_thing
+        return make_json_response(new_thing)
 
 @app.route('/things/<thing_id>',  methods=['PUT'])
 def update_thing(thing_id):
-    thing = request.json
-    things.update({thing['id']: thing})
-    return make_json_response({'message': 'OK'})
+    with database() as things:
+        thing = request.json
+        things.update({thing['id']: thing})
+        return make_json_response({'message': 'OK'})
 
 @app.route('/things/<thing_id>',  methods=['DELETE'])
 def delete_thing(thing_id):
-    thing = request.json
-    del things[thing_id]
-    return make_json_response({'message': 'OK'})
+    with database() as things:
+        del things[str(thing_id)]
+        return make_json_response({'message': 'OK'})
 
 
 if __name__ == '__main__':
