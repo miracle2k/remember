ba3f0d48fe7dd8f6b053e1a1a9ad68d7e2bd8da4
Michael Elsdörfer <michael@elsdoerfer.com>
2014-02-23 14:37:52 +0100

    Make Sass use UTF8 to compile.

--- client/update-css.sh
+++ client/update-css.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 if [ "$1" = "--update" ]; then
-    sass --update css-src:stylesheets
+    sass --update css-src:stylesheets -E "UTF-8"
 else
-    sass --watch css-src:stylesheets
+    sass --watch css-src:stylesheets -E "UTF-8"
 fi
