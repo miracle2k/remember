6c1e7fa89f314faa597bcdb2926d52d423e6aefa
Michael Elsdörfer <michael@elsdoerfer.com>
2014-02-23 10:35:03 +0100

    Simplify heroku config.

--- server/heroku_config.py
+++ server/heroku_config.py
@@ -1,9 +1,6 @@
 from os import environ
-from os.path import join, dirname
-THIS = dirname(__file__)
+
+from config import *
 
 USE_SHOVE = True
 DATABASE = environ.get('DATABASE_URL')
-
-DEBUG = False
-CLIENT_PATH = join(THIS, '..', 'client')
